cf4ff4a31539974e2359eb4a570322a3c968d6e4
cunnellp5 <philip.cunnell@colorado.edu>
2017-05-30 13:54:24 -0600

    profile build ready

--- src/components/bike/map.vue
+++ src/components/bike/map.vue
@@ -1,26 +1,31 @@
 <template lang="html">
   <div id="app">
-    <h1>Map view</h1>
+    <h1 class="pad">Map view</h1>
+    <span v-if="loading" class="mids">
+      <spinner name="circles" color="#5ac6ce" :size="200"></spinner>
+    </span>
+    <span v-if="!loading">
+      <gmap-map class="pad"
+        :center="center"
+        :zoom="12"
+        style="width: 100%; height: 300px"
+        >
+        <gmap-marker
+          v-for="m in markers"
+          :position="m.geometry.location"
+          :clickable="true"
+          :draggable="true"
+          :scroll="false"
+          @click="center=m.position"
+        ></gmap-marker>
+      </gmap-map>
 
-    <gmap-map
-      :center="center"
-      :zoom="12"
-      style="width: 100vw; height: 300px"
-      >
-      <gmap-marker
-        v-for="m in markers"
-        :position="m.geometry.location"
-        :clickable="true"
-        :draggable="true"
-        @click="center=m.position"
-      ></gmap-marker>
-    </gmap-map>
-
-    <div>
-      <ul>
-        <li v-for="info in maps"> {{ info.name }}</li>
-      </ul>
-    </div>
+      <div class="pad">
+        <ul>
+          <li v-for="info in maps"> {{ info.name }}</li>
+        </ul>
+      </div>
+    </span>
   </div>
 </template>
 <script>
@@ -29,20 +34,40 @@ import axios from 'axios'
 export default {
   data () {
     return {
+      loading: true,
+      currentLocation: {
+        currentLocationLat: null,
+        currentLocationLon: null,
+        currentLocation: null
+      },
       maps: [],
-      center: { lat: 39.74, lng: -104.99 },
+      center: { lat: 39.757600, lng: -105.0069376000 },
       markers: [{
         position: {}
       }]
     }
   },
   mounted () {
+    console.log(this.center)
     axios.get('http://localhost:3000/map')
     .then((response) => {
       this.maps = response.data
       this.markers = response.data
       console.log(response.data)
+    }).then(() => {
+      this.loading = false
     })
+    if ('geolocation' in navigator) {
+      var gl = navigator.geolocation
+      gl.getCurrentPosition(function (position) {
+        this.currentLocation = position.coords
+        this.currentLocationLon = position.coords.longitude
+        this.currentLocationLat = position.coords.latitude
+        console.log(this.currentLocation)
+        this.center.lat = this.currentLocationLat
+        this.center.lng = this.currentLocationLon
+      }.bind(this)) // bind to `this` so it's the current component.
+    }
   }
 }
 </script>
@@ -52,5 +77,12 @@ export default {
   .map-container {
     width: 500px;
     height: 300px;
+  }
+  .pad {
+    padding: 25px;
+  }
+  .mids {
+    display: flex;
+    justify-content: center;
   }
 </style>
--- src/components/bike/profileBike.vue
+++ src/components/bike/profileBike.vue
@@ -1,17 +1,33 @@
 <template lang="html">
-  <div class="">
+  <div class="bod">
     <h1>Profile view</h1>
+    <li v-for="info in user"> {{ info.username }}</li>
     <!-- <div>
       {{ this.$store.state.myValue }}
     </div>
     <br>
     <button @click="decrement" type="button" name="button" class="secondary width-1of5">-</button>
     <button @click="increment" type="button" name="button" class="primary width-1of5">+</button> -->
+    <q-ajax-bar></q-ajax-bar>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data () {
+    return {
+      user: []
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:3000/users')
+    .then((response) => {
+      this.user = response.data.Users
+      console.log(response.data.Users)
+    })
+  }
   // methods: {
   //   increment () {
   //     this.$store.dispatch('increment')
@@ -24,4 +40,7 @@ export default {
 </script>
 
 <style lang="css">
+  .bod {
+    padding: 25px;
+  }
 </style>
